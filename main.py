4c9c7b11525b11e396be065c88e21f53a77941f1
Rafal Krolik <krolikk.rafal@gmail.com>
2023-09-20 23:26:48 +0200

    feat: Add game logic
    Implemented the core logic for the Rock, Paper, Scissors game, which includes user input and nested if statements to determine the winner.
    Now, players can play the game and receive outcomes based on their choices.
    
    This commit introduces the following changes:
    - Added user input for selecting Rock, Paper, or Scissors.
    - Implemented logic to generate a random choice for the computer.
    - Created nested if statements to determine the game's outcome.
    - Displayed the result of each round to the user.

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ import random as ra
 # emoji: rock, paper, scissors
 player_one = ["\U0001F91C", "\U0001FAF1", "\U0000270C"]
 player_two = ["\U0001F91B", "\U0001FAF2", "\U0000270C"]
+
+player_choce = int(input("What do you choose? type 0 for Rock, 1 for Paper or 2 for Scissors.\t"))
+
+if player_choce < 0 or player_choce >3:
+    print("Wrong number, please try again")
+else:
+    computer_choce = ra.randint(0, 2)
+
+
+    print(f"Your chose: {player_one[player_choce]}  VS {player_two[computer_choce]}  :Computer choce")
+
+    if (player_choce == 0 and computer_choce == 2) or (player_choce == 1 and computer_choce == 0) or (player_choce == 2 and computer_choce == 1):
+        print("You Win!")
+    elif player_choce == computer_choce:
+        print("Draw!")
+    else:
+        print("You Lose!")
